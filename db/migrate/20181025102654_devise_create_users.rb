b95d1a77bd3583b765cad10f4c62b0578c2681f4
EC2 Default User <ec2-user@ip-172-31-46-123.ap-southeast-1.compute.internal>
2018-11-20 11:12:30 +0000

    update6

--- db/migrate/20181025102654_devise_create_users.rb
+++ db/migrate/20181025102654_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email
-      t.string :encrypted_password
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[4.2]
       # t.datetime :locked_at
 
 
-      t.timestamps
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
